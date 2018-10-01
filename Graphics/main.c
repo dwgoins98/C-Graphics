5d93ff351f1da44d88b86d43512454893631fb84
Daniel Goins <dwgoins98@gmail.com>
2018-10-01 15:34:38 -0400

    Initial Commit

---
+++ Graphics/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Graphics
+//
+//  Created by Daniel Goins on 10/1/18.
+//  Copyright © 2018 Daniel Goins. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
